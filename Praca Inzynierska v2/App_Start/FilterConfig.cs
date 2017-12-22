cc6385c48efd20a3450c544b6d699e5531cfef9a
jaszczykwojciech <jaszczykwojciech@gmail.com>
2017-12-22 16:13:06 +0100

    Dodaj pliki projektów.

---
+++ Praca Inzynierska v2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Praca_Inzynierska_v2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
